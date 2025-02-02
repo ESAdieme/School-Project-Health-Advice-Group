09177aea9fe520dc85d33a4c9efa37e7cf5e27f7
Feam <145436462+ESAdieme@users.noreply.github.com>
2025-02-02 16:19:01 +0000

    Update 2.0.2 - Minor Error Page Changes
    Changed formatting and added a space for the error message

--- services/error.php
+++ services/error.php
@@ -4,22 +4,25 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>404 Page Not Found</title>
-  <link rel="stylesheet" href="../error/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Page Not Found</title>
+    <link rel="stylesheet" href="../error/style.css">
 </head>
+
 <body>
-  <div class="container">
-    <div class="content">
-      <h1>Oops! 404 Error</h1>
-      <p><?php 
+    <div class="container">
+        <div class="content">
+            <h1>Oops! 404 Error</h1>
+            <p><?php 
         $mysqli = require __DIR__ . "/../database.php";
-      echo "Error!". $mysqli->error; 
+      echo "Error! ". $mysqli->error; 
       ?></p>
-      <a href="../index.html" class="back-home">Go Back to Home</a>
+            <a href="../index.html" class="back-home">Go Back to Home</a>
+        </div>
     </div>
-  </div>
 </body>
+
 </html>
